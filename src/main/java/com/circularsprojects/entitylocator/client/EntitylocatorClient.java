e7531f886c81779d1e6c86c0ec9d59f8b4b6c42a
circularsprojects <admin@circularsprojects.com>
2022-10-02 13:55:01 +1100

    piechart detection improvements

--- src/main/java/com/circularsprojects/entitylocator/client/EntitylocatorClient.java
+++ src/main/java/com/circularsprojects/entitylocator/client/EntitylocatorClient.java
@@ -7,7 +7,9 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.ChatHud;
+import net.minecraft.client.option.GameOptions;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.profiler.ProfileResult;
 import net.minecraft.util.profiler.ProfilerTiming;
 
@@ -29,6 +31,15 @@ public class EntitylocatorClient implements ClientModInitializer {
 }
 
 class getEntities extends TimerTask {
+    private void addDebugMessage(Formatting formatting, Text text) {
+        MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.empty().append((Text)Text.translatable("debug.prefix").formatted(formatting, Formatting.BOLD)).append(" ").append(text));
+    }
+    private void debugLog(Text text) {
+        this.addDebugMessage(Formatting.YELLOW, text);
+    }
+    private void debugLog(String key, Object... args) {
+        this.debugLog(Text.translatable(key, args));
+    }
     public void run() {
         try {
 //            MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.translatable(MinecraftClient.getInstance().worldRenderer.getEntitiesDebugString()));
@@ -40,23 +51,25 @@ class getEntities extends TimerTask {
         }
         try {
             ProfileResult profileResult = ((clientProfilerAccessor) MinecraftClient.getInstance()).getTickProfilerResult();
-            String openProfilerSection = ((clientProfilerAccessor) MinecraftClient.getInstance()).getOpenProfilerSection();
+//            String openProfilerSection = ((clientProfilerAccessor) MinecraftClient.getInstance()).getOpenProfilerSection();
 
-            System.out.println(openProfilerSection);
-            System.out.println(profileResult.getTimings(openProfilerSection));
-            // GITHUB COPILOT RAMBLING:
-            // profileResult.getTimings(openProfilerSection) is a list containing several ProfilerTiming objects
-            // each ProfilerTiming object contains:
-            // a string name
-            // a double parentSectionUsagePercentage
-            // a double totalUsagePercentage
-            // get these values and display them as "Name [parentSectionUsagePercentage]/[totalUsagePercentage]"
-            for (ProfilerTiming profilerRes : profileResult.getTimings(openProfilerSection)) {
-                System.out.println();
-                System.out.println(profilerRes.name);
-                System.out.println(profilerRes.parentSectionUsagePercentage);
-                System.out.println(profilerRes.totalUsagePercentage);
-                System.out.println();
+            if (MinecraftClient.getInstance().toggleDebugProfiler(this::debugLog)) {
+                this.debugLog("debug.profiling.start", 10);
+            } else {
+                MinecraftClient.getInstance().toggleDebugProfiler(this::debugLog);
+            }
+            for (ProfilerTiming profilerRes : profileResult.getTimings("root\u001Etick\u001Elevel\u001EblockEntities")) {
+                // detect chest, ender_chest, enchanting_table, shulker_box
+                switch (profilerRes.name) {
+                    case "minecraft:chest" ->
+                            MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.translatable("Detected chest!"));
+                    case "minecraft:ender_chest" ->
+                            MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.translatable("Detected ender chest!"));
+                    case "minecraft:enchanting_table" ->
+                            MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.translatable("Detected enchanting table!"));
+                    case "minecraft:shulker_box" ->
+                            MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.translatable("Detected shulker box!"));
+                }
             }
         } catch(Exception e) {
             System.out.println("[ENTITYLOCATOR] encountered an error in time loop #2!");
